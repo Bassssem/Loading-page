52087e70c6dd6cab82d9d504192843b5813ebe90
Farhani Bassem <fbassem13@gmail.com>
2024-04-21 07:15:17 -1100

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+setTimeout(function() {
+  document.getElementById("loading-page").className += "loaded";
+  document.getElementById("loader").className += "opzero";
+  document.getElementById("lastray").className += " finalray";
+  document.body.className += "whitebk";
+},6500);
